b0fdec089b7e846cb149c73ffbf88a11229506cb
Talhanasar <talhabinnasar7@gmail.com>
2024-07-10 23:53:35 +0600

    Update mongodb-connection.js

--- config/mongodb-connection.js
+++ config/mongodb-connection.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 
 mongoose.connect(`${process.env.MONGODB_URI}/miniproject`)
 .then().catch((err)=>{
-    res.status(404).send(err.message);
+    console.log(err.message);
 });
 
 module.exports = mongoose.connection;
