37f1f15c40aebd1f18d1dcd4c2f135f2becc5ea8
Talhanasar <talhabinnasar7@gmail.com>
2024-07-11 10:24:10 +0600

    Update mongodb-connection.js

--- config/mongodb-connection.js
+++ config/mongodb-connection.js
@@ -8,7 +8,7 @@ mongoose.connect(process.env.MONGODB_URI, { useNewUrlParser: true, useUnifiedTop
     console.log('Connected to MongoDB');
   })
   .catch((error) => {
-    console.error('Error connecting to MongoDB:', error);
+    console.error('Error connecting to MongoDB:', error.message);
   });
 
 
