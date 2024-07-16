d7a3e0e24b980a90be656b14b9e96dd3537b8346
Talhanasar <talhabinnasar7@gmail.com>
2024-07-16 11:54:07 +0600

    update cookie session

--- app.js
+++ app.js
@@ -1,52 +1,59 @@
 const express = require('express');
-const app = express();
-const router = express.Router();
 const path = require('path');
-const cookieParser = require("cookie-parser");
-const expressSession = require("express-session");
-const flash = require("connect-flash");
-require("dotenv").config();
+const cookieParser = require('cookie-parser');
+const expressSession = require('express-session');
+const flash = require('connect-flash');
+require('dotenv').config();
 
 const userRouter = require('./routes/userRouter');
 const profileRouter = require('./routes/profileRouter');
 const blogRouter = require('./routes/blogRouter');
-const userModel = require('./models/userModel');
-const postModel = require('./models/postModel');
 const db = require('./config/mongodb-connection');
 const isLoggedIn = require('./middlewares/isLoggedIn');
+
 const getProfile = require('./middlewares/getProfile');
- 
+
+const app = express();
+
 app.use(cookieParser());
 app.use(express.json());
-app.use(express.urlencoded({extended:true}));
-app.use(express.static(path.join(__dirname,'public')));
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static(path.join(__dirname, 'public')));
 app.use(expressSession({
-   resave: false,
-   saveUninitialized: false,
-   secret: process.env.EXPRESS_SESSION_SECRET,
+    resave: false,
+    saveUninitialized: false,
+    secret: process.env.EXPRESS_SESSION_SECRET,
+    cookie: {
+        secure: true, // Set to true if you're using HTTPS
+        maxAge: 24 * 60 * 60 * 1000 // 1 day default
+    }
 }));
 app.use(flash());
-app.set('view engine','ejs');
-app.use((req,res,next)=>{
+app.set('view engine', 'ejs');
+
+app.use((req, res, next) => {
     res.locals.page = req.path.substring(1) || 'home';
     next();
-}) ;
-
-app.get('/',getProfile,(req,res) => {
-   let message = {
-      error: req.flash("error"),
-      success: req.flash("success")
-   }
-   res.render('home',{message});
 });
-app.get('/about',getProfile,(req,res) => {
-   res.render('about');
+
+app.get('/', getProfile, (req, res) => {
+    let message = {
+        error: req.flash('error'),
+        success: req.flash('success')
+    };
+    res.render('home', { message });
+});
+
+app.get('/about', getProfile, (req, res) => {
+    res.render('about');
 });
 
 app.use('/user', userRouter);
 app.use('/profile', profileRouter);
 app.use('/blog', blogRouter);
- 
-app.listen(3000);
 
-module.exports = router;
+app.listen(3000, () => {
+    console.log('Server is running on port 3000');
+});
+
+module.exports = app;
--- controllers/authController.js
+++ controllers/authController.js
@@ -4,43 +4,46 @@ const { generateToken } = require('../utils/generateToken');
 
 module.exports.loginUser = async (req, res) => {
     try {
-        let {email, password,remember} = req.body;
-        let user = await userModel.findOne({email});
-        if(user){
-            bcrypt.compare(password,user.password,function(err,result){
-                if(!result){
-                    req.flash("error","Invalid Email/Password");
-                    return res.redirect("/user/signin");
-                } 
+        let { email, password, remember } = req.body;
+        let user = await userModel.findOne({ email });
+
+        if (user) {
+            bcrypt.compare(password, user.password, function(err, result) {
+                if (!result) {
+                    req.flash('error', 'Invalid Email/Password');
+                    return res.redirect('/user/signin');
+                }
                 let token = generateToken(user);
-                req.flash("success","Logged in Successfully!");
+                req.flash('success', 'Logged in Successfully!');
+
                 if (remember) {
                     req.session.cookie.maxAge = 15 * 24 * 60 * 60 * 1000; // 15 days
                 } else {
-                    req.session.cookie.expires = false; // Session cookie, expires on browser close
+                    req.session.cookie.expires = false;
                 }
-                res.cookie("token",token);
-                res.redirect("/");
-            });
-        }else{
-            req.flash("error","Invalid Email/Password");
-            res.redirect("/user/signin");
-        }
 
+                res.cookie('token', token);
+                res.redirect('/');
+            });
+        } else {
+            req.flash('error', 'Invalid Email/Password');
+            res.redirect('/user/signin');
+        }
     } catch (err) {
         res.status(404).send(err.message);
     }
-}
+};
+
 module.exports.registerUser = async (req, res) => {
     try {
         let { name, username, email, password } = req.body;
 
         let user = await userModel.findOne({ email });
-        if (user){
-            req.flash("error","Account already exists");
-            return res.redirect("/user/signup");
+        if (user) {
+            req.flash('error', 'Account already exists');
+            return res.redirect('/user/signup');
+        }
 
-        } 
         bcrypt.genSalt(10, (err, salt) => {
             bcrypt.hash(password, salt, async (err, hash) => {
                 if (err) return res.send(err.message);
@@ -51,23 +54,23 @@ module.exports.registerUser = async (req, res) => {
                     email,
                     password: hash
                 });
-                let token = generateToken(user);
-                res.cookie("token", token);
-                req.flash("success","Welcome to talhas-blog!");
-                res.redirect("/")
-            })
-        });
 
+                let token = generateToken(user);
+                res.cookie('token', token);
+                req.flash('success', 'Welcome to talhas-blog!');
+                res.redirect('/');
+            });
+        });
     } catch (err) {
         res.status(404).send(err.message);
     }
-}
+};
 
-module.exports.logoutUser = async (req,res)=>{
-    try{
-        res.cookie("token","");
-        res.redirect("/");
-    }catch(err){
+module.exports.logoutUser = async (req, res) => {
+    try {
+        res.cookie('token', '', { maxAge: 0 });
+        res.redirect('/');
+    } catch (err) {
         res.status(404).send(err.message);
     }
-}
+};
