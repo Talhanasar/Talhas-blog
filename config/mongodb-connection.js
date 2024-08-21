61fafb39ad40b8b19db34d6174f1a854997122b3
Talhanasar <talhabinnasar7@gmail.com>
2024-08-21 23:20:44 +0600

    vercel update

--- config/mongodb-connection.js
+++ config/mongodb-connection.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose');
 
 mongoose.connect(process.env.MONGODB_URI)
-  .then()
+  .then(()=>console.log("DB Connected!"))
   .catch((error) => {
     console.error('Error connecting to MongoDB:', error.message);
   });
